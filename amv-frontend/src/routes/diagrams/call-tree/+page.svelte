fb5c7f4713b13abafae2c18c44ecb21f3d2dc980
tmng1 <kohei.tominaga@monocrea.co.jp>
2025-12-16 17:09:48 +0900

    #14628対応

--- amv-frontend/src/routes/diagrams/call-tree/+page.svelte
+++ amv-frontend/src/routes/diagrams/call-tree/+page.svelte
@@ -3,35 +3,48 @@
   import type { CallTreeCriteriaModel, CallTreeElementModel } from '$lib/arch/api/Api';
   import CheckBox from '$lib/arch/form/CheckBox.svelte';
   import CriteriaUtils from '$lib/arch/search/CriteriaUtils';
+  import type { CriteriaModel } from './+page';
   import { ScrollText, Search } from '@lucide/svelte';
   import type { PageProps } from './$types';
   import ToCallTree from '$lib/domain/diagrams/ToCallTree.svelte';
   import InputField from '$lib/arch/form/InputField.svelte';
 
   let { data }: PageProps = $props();
+  let { criteria: _criteria, methods } = $derived(data);
+  // svelte-ignore state_referenced_locally
+  let criteria = $state(_criteria);
   let { callTrees } = $derived(data);
-  let { signaturePattern, callTreeRequired, calledTreeRequired } = $state(data.criteria);
+  let { signaturePattern, callTreeRequired, calledTreeRequired } = $state(data.criteria as CallTreeCriteriaModel);
   let showExternalPackage = $state(data.showExternalPackage);
   let packageLevel = $state(data.packageLevel);
 
   $effect(() => {
-    search({
-      signaturePattern,
-      callTreeRequired,
-      calledTreeRequired
-    });
+    criteria = _criteria;
   });
 
-  // const form = FormValidator.createForm({}, search);
+  $effect(() => {
+    search(criteria);
+  });
 
-  async function search(criteria: CallTreeCriteriaModel) {
+  $effect(() => {
+    if (callTrees && callTrees.length > 0) {
+      callTreeRequired = true;
+      calledTreeRequired = true;
+    }
+  });
+
+  async function search(criteria: CriteriaModel) {
     await goto(CriteriaUtils.encode(criteria));
+  }
+
+  function url(qualifiedSignature: string | undefined) {
+    return CriteriaUtils.encode({ methodCriteria: { text: qualifiedSignature } });
   }
 
   function isInternalPackage(element: CallTreeElementModel, level: number): boolean {
     return element.elementTags.some((tag) => {
-      if (!tag.startsWith('same-package-')) return false;
-      const n = parseInt(tag.split('-')[2]);
+      if (!tag.startsWith("same-package-")) return false;
+      const n = parseInt(tag.split("-")[2]);
       return n >= level;
     });
   }
@@ -39,152 +52,168 @@
 
 <section class="container">
   <!-- svelte-ignore a11y_autofocus -->
-  <input id="search" type="search" bind:value={signaturePattern} autofocus />
-</section>
+  <input id="search" type="search" bind:value={criteria.methodCriteria.text} autofocus />
 
-{#if callTrees.length > 0}
-  <section>
-    <article>
-      The font of each element in the Call Tree has the following meanings:
-      <div>
-        <span class="internal">Call</span>
-        <span>: Method within target internal package level</span>
-      </div>
-      <div>
-        <span class="data">Call</span>
-        <span>: Class that holds data such as a Dto, an Entity or their Builder ("Data Class")</span
-        >
-      </div>
-      <div>
-        <span class="setter">Call</span>
-        <span class="notation-label">: Setter method of Data Class</span>
-      </div>
-      <div>
-        <span class="getter">Call</span>
-        <span>: Getter method of Data Class</span>
-      </div>
-    </article>
-  </section>
-{/if}
-
-{#each callTrees as callTree}
-  {@const method = callTree.method}
-
-  <section class="container-fluid setting">
-    <CheckBox id="call-tree" label="Call Tree" bind:checked={callTreeRequired} />
-    <CheckBox id="called-tree" label="Called Tree" bind:checked={calledTreeRequired} />
-    <InputField
-      id="internal-package-level"
-      label="Internal Package Level"
-      labelPos="after"
-      type="number"
-      min="1"
-      width="4rem"
-      bind:value={packageLevel}
-    />
-    <CheckBox
-      id="show-external-package"
-      label="Show External Package Methods"
-      bind:checked={showExternalPackage}
-    />
-  </section>
-
-  <section class="container-fluid">
-    <h3>{method.type}.{method.simpleSignature}</h3>
-
-    <p>
-      <strong>Qualified Signature:</strong>
-      {method.qualifiedSignature}
-    </p>
-
-    {#if callTreeRequired}
-      <h4>Call Tree</h4>
-
-      {#each callTree.callTree as e}
-        {@render element(e)}
+  {#if methods.list && methods.list!.length > 1}
+    {@const andMoreCount = methods.pageResult!.count! - methods.list.length}
+    <ul>
+      {#each methods.list as method}
+        <li>
+          <a href={url(method.qualifiedSignature)}>{method.qualifiedSignature}</a>
+        </li>
       {/each}
-    {/if}
 
-    {#if calledTreeRequired}
-      <h4>Called Tree</h4>
-
-      {#each callTree.calledTree as e}
-        {@render element(e)}
-      {/each}
-    {/if}
-  </section>
-{/each}
-
-{#snippet element(element: CallTreeElementModel)}
-  {@const method = element.method}
-  {#if showExternalPackage || isInternalPackage(element, packageLevel)}
-    <div style="margin-left: {element.depth}rem">
-      {#if element.depth > 0}
-        {element.call ? '-> ' : '<- '}
+      {#if andMoreCount > 0}
+        <li>
+          ... and {andMoreCount} more
+        </li>
       {/if}
-
-      <span
-        class={`${
-          isInternalPackage(element, packageLevel) ? 'internal ' : ''
-        }${element.elementTags.join(' ')}`}
-      >
-        {#if method.dummy}
-          <span>{method.name}</span>
-        {:else}
-          <span class={element.typeTags.join(' ')}>{method.type}</span>.<span
-            class={element.methodTags.join(' ')}>{method.simpleSignature}</span
-          >
-        {/if}
-      </span>
-
-      {#if element.lineNoAt > 0}
-        at
-        {#if element.urlAt}
-          <a href={element.urlAt}>L.{element.lineNoAt}</a>
-        {:else}
-          L.{element.lineNoAt}
-        {/if}
-      {/if}
-
-      {#if !method.dummy}
-        <a
-          href={`/types/${method.namespace}.${method.type}#${method.simpleSignature}`}
-          title={method.qualifiedSignature}
-        >
-          <Search />
-        </a>
-      {/if}
-
-      {#if method.srcUrl}
-        <a href={method.srcUrl} title={`Go to the declaration of ${method.simpleSignature}`}>
-          <ScrollText />
-        </a>
-
-        <ToCallTree
-          signaturePattern={method.qualifiedSignature}
-          onclick={() => {
-            signaturePattern = method.qualifiedSignature;
-          }}
-        />
-      {/if}
-    </div>
+    </ul>
   {/if}
-{/snippet}
 
-<style lang="scss">
-  .internal {
-    font-weight: bold;
-  }
+  {#if callTrees.length > 0}
+    <section>
+      <article>
+        The font of each element in the Call Tree has the following meanings:
+        <div>
+          <span class="internal">Call</span>
+          <span>: Method within target internal package level</span>
+        </div>
+        <div>
+          <span class="data">Call</span>
+          <span>: Class that holds data such as a Dto, an Entity or their Builder ("Data Class")</span>
+        </div>
+        <div>
+          <span class="setter">Call</span>
+          <span class="notation-label">: Setter method of Data Class</span>
+        </div>
+        <div>
+          <span class="getter">Call</span>
+          <span>: Getter method of Data Class</span>
+        </div>
+      </article>
+    </section>
+  {/if}
 
-  .data {
-    color: var(--pico-color-green);
-  }
+  {#each callTrees as callTree}
+    {@const method = callTree.method}
 
-  .setter {
-    color: var(--pico-color-orange);
-  }
+    <section class="container-fluid setting">
+      <CheckBox id="call-tree" label="Call Tree" bind:checked={callTreeRequired} />
+      <CheckBox id="called-tree" label="Called Tree" bind:checked={calledTreeRequired} />
+      <InputField
+        id="internal-package-level"
+        label="Internal Package Level"
+        labelPos="after"
+        type="number"
+        min="1"
+        width="4rem"
+        bind:value={packageLevel}
+      />
+      <CheckBox id="show-external-package" label="Show External Package Methods" bind:checked={showExternalPackage} />
+    </section>
 
-  .getter {
-    color: var(--pico-color-indigo);
-  }
-</style>
+    <section class="container-fluid">
+      <h3>{method.type}.{method.simpleSignature}</h3>
+
+      <p>
+        <strong>Qualified Signature:</strong>
+        {method.qualifiedSignature}
+      </p>
+
+      {#if callTreeRequired}
+        <h4>Call Tree</h4>
+
+        {#each callTree.callTree as e}
+          {@render element(e)}
+        {/each}
+      {/if}
+
+      {#if calledTreeRequired}
+        <h4>Called Tree</h4>
+
+        {#each callTree.calledTree as e}
+          {@render element(e)}
+        {/each}
+      {/if}
+    </section>
+  {/each}
+
+  {#snippet element(element: CallTreeElementModel)}
+    {@const method = element.method}
+    {#if showExternalPackage || isInternalPackage(element, packageLevel)}
+      <div style="margin-left: {element.depth}rem">
+        {#if element.depth > 0}
+          {element.call ? "-> " : "<- "}
+        {/if}
+
+        <span class={`${isInternalPackage(element, packageLevel) ? "internal " : ""}${element.elementTags.join(" ")}`}>
+          {#if method.dummy}
+            <span>{method.name}</span>
+          {:else}
+            <span class={element.typeTags.join(" ")}>{method.type}</span>.<span class={element.methodTags.join(" ")}
+              >{method.simpleSignature}</span
+            >
+          {/if}
+        </span>
+
+        {#if element.lineNoAt > 0}
+          at
+          {#if element.urlAt}
+            <a href={element.urlAt}>L.{element.lineNoAt}</a>
+          {:else}
+            L.{element.lineNoAt}
+          {/if}
+        {/if}
+
+        {#if !method.dummy}
+          <a
+            href={`/types/${method.namespace}.${method.type}#${method.simpleSignature}`}
+            title={method.qualifiedSignature}
+          >
+            <Search />
+          </a>
+        {/if}
+
+        {#if method.srcUrl}
+          <a href={method.srcUrl} title={`Go to the declaration of ${method.simpleSignature}`}>
+            <ScrollText />
+          </a>
+
+          <!-- svelte-ignore a11y_no_static_element_interactions -->
+          <!-- svelte-ignore event_directive_deprecated -->
+          <span
+            on:click|preventDefault={() => {
+              criteria.methodCriteria.text = method.qualifiedSignature;
+            }}
+            on:keydown={(e) => {
+              if (e.key === 'Enter' || e.key === ' ') {
+                e.preventDefault();
+                criteria.methodCriteria.text = method.qualifiedSignature;
+              }
+            }}>
+            <ToCallTree signaturePattern={method.qualifiedSignature} />
+          </span>
+        {/if}
+      </div>
+    {/if}
+  {/snippet}
+
+  <style lang="scss">
+    .internal {
+      font-weight: bold;
+    }
+
+    .data {
+      color: var(--pico-color-green);
+    }
+
+    .setter {
+      color: var(--pico-color-orange);
+    }
+
+    .getter {
+      color: var(--pico-color-indigo);
+    }
+  </style>
+</section>
--- amv-frontend/src/routes/diagrams/call-tree/+page.ts
+++ amv-frontend/src/routes/diagrams/call-tree/+page.ts
@@ -1,29 +1,80 @@
-import type { CallTreeCriteriaModel, CallTreeModel } from '$lib/arch/api/Api';
 import ApiHandler from '$lib/arch/api/ApiHandler';
-import CriteriaUtils from '$lib/arch/search/CriteriaUtils';
+import type {
+  InteractionDocumentCriteriaModel,
+  MethodSearchCriteriaModel,
+  MethodSearchResultModel,
+  CallTreeModel
+} from '$lib/arch/api/Api';
 import type { PageLoad } from './$types';
+import CriteriaUtils from '$lib/arch/search/CriteriaUtils';
+
+export type CriteriaModel = {
+  methodCriteria: MethodSearchCriteriaModel;
+  documentCriteria: InteractionDocumentCriteriaModel;
+  init: boolean;
+};
+
+export type CallTreeCriteriaModel = {
+  signaturePattern?: string;
+  callTreeRequired?: boolean;
+  calledTreeRequired?: boolean;
+  render?: string;
+  /** @format int32 */
+  limit?: number;
+};
 
 export const load: PageLoad = async ({ fetch, url }) => {
+  const decoded = CriteriaUtils.decode(url);
+  const criteria = {
+    methodCriteria: {
+      text: decoded.signaturePattern ?? decoded.methodCriteria?.text
+    },
+    documentCriteria: {
+      participableStereotypes: []
+    },
+    init: true,
+    ...CriteriaUtils.decode(url)
+  } as CriteriaModel;
+
+  const methods = await searchMethods(criteria.methodCriteria, fetch);
+
+  criteria.documentCriteria.qualifiedSignature = methods.list?.length === 1 ? methods.list[0].qualifiedSignature! : "";
+
+  let callTrees: CallTreeModel[] = [];
+
+  if (Array.isArray(methods.list) && methods.list.length === 1) {
+    callTrees = await getCallTree(url, methods.list[0].qualifiedSignature);
+  }
+
+  return {
+    criteria,
+    methods,
+    showExternalPackage: false,
+    packageLevel: 3,
+    callTrees
+  };
+};
+
+async function searchMethods(criteria: MethodSearchCriteriaModel, fetch: typeof window.fetch) {
+  if (!criteria.text || criteria.text.length < 2) {
+    return {} as MethodSearchResultModel;
+  }
+
+  return (
+    (await ApiHandler.handle<MethodSearchResultModel>(fetch, (api) => api.methods.search(criteria))) ||
+    ({} as MethodSearchResultModel)
+  );
+}
+
+async function getCallTree(url: URL, signaturePattern: string | undefined) {
   const criteria = {
     callTreeRequired: true,
     calledTreeRequired: true,
-    render: 'HTML',
-    limit: 5,
+    render: "HTML",
+    limit: 10,
+    signaturePattern: signaturePattern,
     ...CriteriaUtils.decode(url)
   } as CallTreeCriteriaModel;
 
-  const open = CriteriaUtils.decodeParam<boolean>(url, 'open') ?? false;
-
-  const callTrees =
-    (await ApiHandler.handle<CallTreeModel[]>(fetch, (api) => api.diagrams.callTree(criteria))) ||
-    [];
-
-  return {
-    title: 'Call Tree',
-    criteria,
-    open,
-    callTrees,
-    showExternalPackage: false,
-    packageLevel: 3
-  };
-};
+  return (await ApiHandler.handle<CallTreeModel[]>(fetch, (api) => api.diagrams.callTree(criteria))) || [];
+}
